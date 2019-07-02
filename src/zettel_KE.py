d391e25567721a7b41d1cf772a25b8abcffb0b6e
Sean Higgins <shiggins@luc.edu>
2019-07-02 15:38:15 -0500

    updated zettel_KE class with new functinos for count dictionaries and word scores

--- src/zettel_KE.py
+++ src/zettel_KE.py
@@ -1,7 +1,11 @@
 import operator
 import numpy as np
+import zettel_preprocessor as process
+import re
 
 class KE(object):
+
+    # def __init__(self):
 
     # def __init__(self, stop_words, regex='[\W\n]+'):
     #     #lets users call predefined stopwords easily in a platform agnostic manner or use their own list
@@ -9,7 +13,33 @@ class KE(object):
     #         self.__stop_words_pattern = build_stop_word_regex(stop_words)
     #     else:
     #         self.__stop_words_pattern = build_stop_word_regex(load_stop_words(stop_words, regex))
-    #
+
+    # run -> tokenize, POS, lemmatize, bi-gram/tri-gram swaps, word scores, keyword scores, tf_idf, text rank, weight distribution...
+    def run(self, zettels): #TODO fix self references
+        z_process = process.ZettelPreProcessor()
+        z_process.init_zettels(zettels)
+
+        tokens = z_process.tokens
+        pos_tokens = z_process.pos_tagged_tokens
+        lemma_tokens = z_process.lemmatized_tokens
+        bi_grams = z_process.bi_gram #TODO
+        tri_grams = z_process.tri_gram #TODO
+
+        doc_count_dict = self.create_doc_count_dictionary(lemma_tokens, z_process.create_unique_corpus())
+        TF_IDF = self.tf_idf(lemma_tokens, doc_count_dict)
+
+        word_scores = self.create_word_score(lemma_tokens) #TODO
+        keyword_scores = self.create_keyword_score(lemma_tokens, word_scores, 1) #TODO
+
+        print(tokens)
+        print(pos_tokens)
+        print(lemma_tokens)
+        print(bi_grams)
+        print(tri_grams)
+        print(doc_count_dict)
+        print(TF_IDF)
+        print(word_scores)
+        print(keyword_scores)
     # def run(self, text, minCharacters=1, maxWords=5, minFrequency=1):
     #     sentence_list = split_sentences(text)
     #
@@ -22,15 +52,22 @@ class KE(object):
     #     sorted_keywords = sorted(keyword_candidates.items(), key=operator.itemgetter(1), reverse=True)
     #     return sorted_keywords
 
-    # run -> tokenize, POS, bi-gram/tri-gram, stopwords, stem/lemma, word scores, keyword scores, tf_idf
-    # word score = deg(word) / freq(word) -- deg(word) = word_list_len - 1 for each word in the phrase + freq(word)
-    # keyword score = sum of each word score in phrase
 
     # page rank = if zettel A has link to zettel B, can be represented as a direct edge from A to B
     # after creating graph, weights = (1-d) + d * ( dot( graph, page_ranks) )
     # initialize page ranks as 1
-    # normalize graph
+    def create_page_rank(self, graph):
+        new_graph = np.asarray(self.normalize_graph(graph))
+        page_ranks = np.full((len(new_graph)), 1)
+        d = 0.85
+        iter = 0
+        for itme in new_graph:
+            iter += 1
+            page_ranks = (1-d) + d * np.dot(new_graph, page_ranks)
+            print(iter)
+            print(page_ranks)
 
+    # normalize graph
     def normalize_graph(self, graph):
         col_index = 0
         for col in graph:
@@ -46,23 +83,97 @@ class KE(object):
             col_index += 1
         return graph
 
-    def create_page_rank(self, graph):
-        new_graph = np.asarray(self.normalize_graph(graph))
-        page_ranks = np.full((len(new_graph)), 1)
-        d = 0.85
-        iter = 0
+    def tf_idf(self, lemma_tokens, doc_count_dict):
+        """ tf_idf = tf * idf """
+        total_docs = len(lemma_tokens)
+        all_tf_idf = []
+        row_length = 0.0
+        for zettel in lemma_tokens:
+            total_words = len(zettel)
+            tf_idf = []
+            count_dict = self.create_count_dictionary(zettel)
+            for word in zettel:
+                # tf = (count of given word for a given zettel) / (total number of words for given zettel)
+                tf = count_dict[word] / total_words
+                # idf = (total number of documents) / (number of documents containing word)
+                idf = total_docs / doc_count_dict[word]
+                tf_idf_value = tf * idf
+                tf_idf.append(tf_idf_value)
+            if row_length < len(tf_idf):
+                row_length = len(tf_idf)
+            all_tf_idf.append(tf_idf)
+        for row in all_tf_idf:
+            while len(row) < row_length:
+                row.append(0.0)
+        return all_tf_idf
 
-        for row in g:
-            for item in row:
-                iter += 1
-                page_ranks = (1-d) + d * np.dot(new_graph, page_ranks)
-                print(iter)
-                print(page_ranks)
+    def create_count_dictionary(self, tokens):
+        word_count_dict = {}
+        for word in tokens:
+            word_count_dict.setdefault(word, 0)
+            word_count_dict[word] += 1
+        return word_count_dict
+
+    def create_doc_count_dictionary(self, lemma_tokens, unique_lemmas):
+        doc_count_dict = {}
+        for zettel in lemma_tokens:
+            for word in unique_lemmas:
+                for token in zettel:
+                    if token == word:
+                        doc_count_dict.setdefault(word, 0)
+                        doc_count_dict[word] += 1
+                        break
+        return doc_count_dict
+
+    # https://github.com/fabianvf/python-rake/blob/master/RAKE/RAKE.py
+    # word score = deg(word) / freq(word) -- deg(word) = word_list_len - 1 for each word in the phrase + freq(word)
+    # single word = 0 degree, bi-gram = 1 degree, tri-gram = 2 degree #TODO experiment with diff numbers
+    def create_word_score(self, lemma_tokens):
+        word_freq = {}
+        word_deg = {}
+        for zettel in lemma_tokens:
+            for word in zettel:
+                word_list = re.split(" ", word)
+                word_list_deg = len(word_list) - 1
+                for new_word in word_list:
+                    word_freq.setdefault(new_word, 0)
+                    word_freq[new_word] = word_freq[new_word] + 1
+                    word_deg.setdefault(new_word, 0)
+                    word_deg[new_word] = word_deg[new_word] + word_list_deg
+        word_score = {}
+        for word in word_freq:
+            word_deg[word] = word_deg[word] + word_freq[word]
+            word_score.setdefault(word, 0)
+            word_score[word] = word_deg[word] / (word_freq[word] * 1.0)
+        return word_score
+
+    # https://github.com/fabianvf/python-rake/blob/master/RAKE/RAKE.py
+    # keyword score = sum of each word score in phrase
+    def create_keyword_score(self, lemma_tokens, word_score, min_freq):
+        keywords_score = {}
+        for zettel in lemma_tokens:
+            for word in zettel:
+                if zettel.count(word) >= min_freq:
+                    keywords_score.setdefault(word, 0)
+                    word_list = re.split(" ", word)
+                    score = 0
+                    for new_word in word_list:
+                        score += word_score[new_word]
+                    keywords_score[word] = score
+        return keywords_score
 
 
 ke = KE()
-g = [[0, 0, 0.5, 0],
-     [0, 0, 0.5, 1],
-     [1, 0.5, 0, 0],
-     [0, 0.5, 0, 0]]
-ke.create_page_rank(g)
+# g = [[0, 0, 0.5, 0],
+#      [0, 0, 0.5, 1],
+#      [1, 0.5, 0, 0],
+#      [0, 0.5, 0, 0]]
+# ke.create_page_rank(g)
+
+rheingold = "/Users/SeanHiggins/ZTextMiningPy/docs/data/zettels/rheingold-examples"
+baseball = "/Users/SeanHiggins/ZTextMiningPy/docs/data/zettels/baseball"
+
+z_process = process.ZettelPreProcessor()
+zettels = z_process.get_zettels_from_directory(baseball)
+
+ke.run(zettels)
--- src/zettel_preprocessor.py
+++ src/zettel_preprocessor.py
@@ -14,51 +14,69 @@ class ZettelPreProcessor:
 	def init_zettels(self, zet):
 		self.zettels = zet
 		self.tokens = self.tokenizer()
-		self.pos_tagged_tokens = self.pos_tagger()
 		self.filtered_words = self.remove_stop_words()
-		# stemmer types: 'porter', 'lancaster', 'snowball'
-		self.stemmed_tokens = self.stemmer('lancaster')
+		self.pos_tagged_tokens = self.pos_tagger()
 		self.lemmatized_tokens = self.lemmatizer()
+		self.stemmed_tokens = self.stemmer('lancaster')  # stemmer types: 'porter', 'lancaster', 'snowball'
 		self.unique_corpus = self.create_unique_corpus()
-		self.n_gram = self.create_n_gram(2)
+		self.bi_gram = self.create_n_gram(2)
+		self.tri_gram = self.create_n_gram(3)
 		self.unique_count_matrix = self.create_count_matrix()
 		#self.unique_tag_corpus = self..create_unique_tag_corpus(tags)  #TODO get tokens from an array and parse...
 		#self.tag_boolean_matrix = self.create_boolean_tag_matrix(unique_tag_corpus)
 		#self.tag_count_matrix = self.create_unique_corpus(unique_tag_corpus)
 
 	def tokenizer(self):
-		tokens = []
+		all_tokens = []
 		for zettel in self.zettels:
+			tokens = []
 			new_list = re.split('\W+', str(zettel))
 			for token in new_list:
 				tokens.append(token)
-		tokens = list(filter(None, tokens))
-		return tokens
-
-	def pos_tagger(self):
-		tags = nltk.pos_tag(self.tokens)
-		tokens_with_pos_tags = []
-		for pair in tags:
-			if pair[1].startswith('J'):
-				tokens_with_pos_tags.append([pair[0], 'a'])
-			elif pair[1].startswith('V'):
-				tokens_with_pos_tags.append([pair[0], 'v'])
-			elif pair[1].startswith('N'):
-				tokens_with_pos_tags.append([pair[0], 'n'])
-			elif pair[1].startswith('R'):
-				tokens_with_pos_tags.append([pair[0], 'r'])
-		return tokens_with_pos_tags
+			tokens = list(filter(None, tokens))
+			all_tokens.append(tokens)
+		return all_tokens
 
 	def remove_stop_words(self):
-		filtered_words = []
-		stop_words = set(stopwords.words('english'))
-		for word in self.pos_tagged_tokens:
-			if word[0].lower() in stop_words:
-				continue
-			else:
-				filtered_words.append(word)
-		filter(None, filtered_words)
-		return filtered_words
+		all_filtered_words = []
+		stop_words = set(stopwords.words('english')) #TODO possibly switch out with own list of stopwords
+		for zettel in self.tokens:
+			filtered_words = []
+			for word in zettel:
+				if word.lower() in stop_words:
+					continue
+				else:
+					filtered_words.append(word)
+			filter(None, filtered_words)
+			all_filtered_words.append(filtered_words)
+		return all_filtered_words
+
+	def pos_tagger(self):
+		all_tokens_with_pos_tags = []
+		for zettel in self.filtered_words:
+			tags = nltk.pos_tag(zettel)
+			tokens_with_pos_tags = []
+			for word in tags:
+				if word[1].startswith('J'):
+					tokens_with_pos_tags.append([word[0], word[1], 'a'])
+				elif word[1].startswith('V'):
+					tokens_with_pos_tags.append([word[0], word[1], 'v'])
+				elif word[1].startswith('N'):
+					tokens_with_pos_tags.append([word[0], word[1], 'n'])
+				elif word[1].startswith('R'):
+					tokens_with_pos_tags.append([word[0], word[1], 'r'])
+			all_tokens_with_pos_tags.append(tokens_with_pos_tags)
+		return all_tokens_with_pos_tags
+
+	def lemmatizer(self):
+		all_lemmatized_tokens = []
+		lemmatizer = WordNetLemmatizer()
+		for zettel in self.pos_tagged_tokens:
+			lemmatized_tokens = []
+			for word in zettel:
+				lemmatized_tokens.append(lemmatizer.lemmatize(word[0], word[2]))
+			all_lemmatized_tokens.append(lemmatized_tokens)
+		return all_lemmatized_tokens
 
 	def stemmer(self, stemmer_type):
 		switch = {
@@ -67,51 +85,48 @@ class ZettelPreProcessor:
 			'snowball': SnowballStemmer('english'),
 		}
 		stemmer = switch.get(stemmer_type)
-		stemmed_tokens = []
-		for word in self.filtered_words:
-			stemmed_tokens.append([stemmer.stem(word[0]), word[1]])
-		return stemmed_tokens
-
-	def lemmatizer(self):
-		lemmatized_tokens = []
-		lemmatizer = WordNetLemmatizer()
-		for word in self.stemmed_tokens:
-			lemmatized_tokens.append(lemmatizer.lemmatize(word[0], word[1]))
-		return lemmatized_tokens
+		all_stemmed_tokens = []
+		for zettel in self.pos_tagged_tokens:
+			stemmed_tokens = []
+			for word in zettel:
+				stemmed_tokens.append([stemmer.stem(word[0]), word[2]])
+			all_stemmed_tokens.append(stemmed_tokens)
+		return all_stemmed_tokens
 
 	def create_unique_corpus(self):
 		unique_corpus = []
-		for word in self.lemmatized_tokens:
-			if word not in unique_corpus:
-				unique_corpus.append(word)
+		for zettel in self.lemmatized_tokens:
+			for word in zettel:
+				if word not in unique_corpus:
+					unique_corpus.append(word)
 		unique_corpus = list(filter(None, unique_corpus))
 		unique_corpus.sort()
 		return unique_corpus
 
-	def create_unique_tag_corpus(self): #TODO fix once tags are recieved correctly
-		unique_tag_corpus = []
-		lock = 0
-		for word in self.tokens:
-			if word == "tags":
-				lock = 1
-				continue
-			if word == "cite":
-				lock = 0
-			while lock == 1:
-				if word not in unique_tag_corpus:
-					unique_tag_corpus.append(word)
-				break
-		unique_tag_corpus = list(filter(None, unique_tag_corpus)).sort()
-		return unique_tag_corpus
+	# def create_unique_tag_corpus(self): #TODO fix once tags are recieved correctly
+	# 	unique_tag_corpus = []
+	# 	lock = 0
+	# 	for word in self.tokens:
+	# 		if word == "tags":
+	# 			lock = 1
+	# 			continue
+	# 		if word == "cite":
+	# 			lock = 0
+	# 		while lock == 1:
+	# 			if word not in unique_tag_corpus:
+	# 				unique_tag_corpus.append(word)
+	# 			break
+	# 	unique_tag_corpus = list(filter(None, unique_tag_corpus)).sort()
+	# 	return unique_tag_corpus
 
-	def create_count_matrix(self):
+	def create_count_matrix(self): #TODO check
 		count_matrix = []
 		for zettel in self.zettels:
 			count = ZettelPreProcessor.get_word_count(self, zettel)
 			count_matrix.append(count)
 		return count_matrix
 
-	def get_word_count(self, zettel):
+	def get_word_count(self, zettel): #TODO check
 		new_unique_corpus = self.unique_corpus
 		count = np.zeros(len(new_unique_corpus))
 		split_zettel = re.split("\W+", str(zettel).lower())
@@ -124,72 +139,75 @@ class ZettelPreProcessor:
 				i += 1
 		return count.tolist()
 
-	def create_boolean_tag_matrix(self): 	#TODO
-		unique_tag_count_matrix = ZettelPreProcessor.create_count_matrix(self)
-		tag_boolean_matrix = []
-		for row in unique_tag_count_matrix:
-			inner_boolean = []
-			for count in row:
-				if count == 0:
-					inner_boolean.append(False)
-				else:
-					inner_boolean.append(True)
-			tag_boolean_matrix.append(inner_boolean)
-		return tag_boolean_matrix
+	# def create_boolean_tag_matrix(self): 	#TODO
+	# 	unique_tag_count_matrix = ZettelPreProcessor.create_count_matrix(self)
+	# 	tag_boolean_matrix = []
+	# 	for row in unique_tag_count_matrix:
+	# 		inner_boolean = []
+	# 		for count in row:
+	# 			if count == 0:
+	# 				inner_boolean.append(False)
+	# 			else:
+	# 				inner_boolean.append(True)
+	# 		tag_boolean_matrix.append(inner_boolean)
+	# 	return tag_boolean_matrix
 
 	def create_n_gram(self, n):
-		n_grams = []
-		for index in range(len(self.lemmatized_tokens)-n+1):
-			pair = self.lemmatized_tokens[index:index+n]
-			split = pair[0] + " " + pair[1]
-			n_grams.append(split)
-		return n_grams
+		all_n_grams = []
+		for zettel in self.tokens:
+			n_grams = []
+			for index in range(len(zettel)-n+1):
+				set = zettel[index:index+n]
+				split = ""
+				for i in range(n):
+					split += set[i]
+					if i < n-1:
+						split = split + " "
+				n_grams.append(split)
+			all_n_grams.append(n_grams)
+		return all_n_grams
 
-	def create_count_dictionary(self):
-		word_count_dict = {}
-		for word in self.lemmatized_tokens:		#TODO tokens or lemmatized_tokens
-			if word in word_count_dict:
-				word_count_dict[word] += 1
-			else:
-				word_count_dict[word] = 1
-		return word_count_dict
-
-	def create_doc_count_dictionary(self):
-		doc_count_dict = {}
-		for zettel in self.zettels:
-			process = ZettelPreProcessor()
-			process.init_zettels(zettel)
-			cur_zettel_dict = {key: 1 for key in process.lemmatized_tokens}		#TODO tokens or lemmatized tokens
-			word_dict = {}
-			for word in self.lemmatized_tokens:
-				if word in word_dict:
-					continue
-				if word in cur_zettel_dict:
-					if word in doc_count_dict:
-						doc_count_dict[word] += 1
-						word_dict[word] = 1
-					else:
-						doc_count_dict[word] = 1
-						word_dict[word] = 1
-		return doc_count_dict
+	# def create_count_dictionary(self, tokens): #TODO replaced?
+	# 	word_count_dict = {}
+	#         for word in tokens:
+	#             word_count_dict.setdefault(word, 0)
+	#             word_count_dict[word] += 1
+	#         return word_count_dict
+	#
+	# def create_doc_count_dictionary(self):
+	# 	doc_count_dict = {}
+	# 	for zettel in self.zettels:
+	# 		process = ZettelPreProcessor()
+	# 		process.init_zettels(zettel)
+	# 		cur_zettel_dict = {key: 1 for key in process.lemmatized_tokens}
+	# 		word_dict = {}
+	# 		for word in self.lemmatized_tokens:
+	# 			if word in word_dict:
+	# 				continue
+	# 			if word in cur_zettel_dict:
+	# 				if word in doc_count_dict:
+	# 					doc_count_dict[word] += 1
+	# 					word_dict[word] = 1
+	# 				else:
+	# 					doc_count_dict[word] = 1
+	# 					word_dict[word] = 1
+	# 	return doc_count_dict
 
 	def get_zettels_from_directory(self, directory):
 		new_zettels = []
 		files = os.listdir(directory)
 		for file in files:
 			path = directory + "/" + file
-			contents = str([line.rstrip() for line in open(path)])
+			contents = [str([line.rstrip() for line in open(path)])]
 			new_zettels.append(contents)
 		return new_zettels
 
 	def get_document_word_counts(self):
 		i = 0
 		counts = []
-		for zettel in self.zettels:
+		for zettel in self.tokens:
 			counts.append(0)
-			process = ZettelPreProcessor()
-			process.init_zettels(zettel)
-			for word in process.tokens:
+			for word in zettel:
 				counts[i] = counts[i] + 1
 			i += 1
 		return counts
